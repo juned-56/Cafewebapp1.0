2c545ff94b8f9cc9ca88dab9c11e58d2a36100b9
juned-56 <junednite@gmail.com>
2023-09-25 19:03:40 +0530

    Testing commit

--- src/main/java/com/cf/web/app/Application.java
+++ src/main/java/com/cf/web/app/Application.java
@@ -9,6 +9,8 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("This is the main class");
+		
+		System.out.println("This is the main class===================");
 	}
 
 }
