ff2a9f2d98f7da1be69d6326eb92d393542ee61e
juned-56 <junednite@gmail.com>
2023-10-06 23:11:31 +0530

    Adding some fetures

--- src/main/java/com/cf/web/app/Application.java
+++ src/main/java/com/cf/web/app/Application.java
@@ -3,11 +3,15 @@ package com.cf.web.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
 	}
 
 }
